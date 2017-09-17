b96b4a9846c910aaa05b186df6f6f911e9970141
ashtrie <aleksandersher@gmail.com>
2017-09-17 20:40:20 +0300

    a scottish man has no clue how to test with zombie and mocha but i bet i do

--- test.js
+++ test.js
@@ -1,12 +1,29 @@
-import Zombie from 'zombie';
-import should from 'should';
-const URL = 'https://kodaktor.ru/g/testing_f38ac';
-const page = new Zombie();
-const g=URL=>new Promise((rs,rj)=>page.visit(URL,e=>(e)?rj(e):rs()));
-const data = [ {"value": "il@gmail.com", "expected": "no"}, {"value": "ilya@gmail.com", "expected": "yes"}, {"value": "elias@gmail.ru", "expected": "no"}, {"value": "il-ya@gmail.com", "expected": "no"}, {"value": "name#gmail.com", "expected": "no"}, {"value": "hahaha@gmail.com", "expected": "yes"} ];
-for (const o of data) {
-  it('should return yes or no', ()=>{
-    g(URL+'?test='+o.value).then(()=>({result: page.document.querySelector('h3#response').textContent})).should
-    .eventually.have.property('result',  o.value );
-  });
-}
+const Zombie = require('zombie');
+const should = require('should');
+const mocha = require('mocha');
+const testValues = require('./testValues');
+
+const url = 'https://katysheva.github.io/Web-tech-task';
+
+testValues.forEach(o => {
+    let requestUrl = `${url}?test=${o.value}`;
+    describe(`A scottish man comes in the pub ${requestUrl}`, () => {
+        const browser = new Zombie();
+        const getResult = () => ({ response: browser.document.querySelector('h3#response').textContent.toLowerCase() });
+        
+        before(done => {
+            browser.visit(requestUrl, done);
+        });
+    
+        describe('pushes an oak damn door', () => {
+    
+            before(done => {
+                browser.pressButton('#startBtn', done);
+            });
+
+            it('and sees how the chap takes a knife and sings "Oh, I have all my fingers\nThe knife goes chop chop chop\nIf I miss the spaces in-between my fingers will come off\nAnd if I hit my fingers\nThe blood will soon come out\nBut all the same I play this game cause that is what it is all about\nOh, chop chop chop chop chop chop\nI am picking up the speed\nAnd if I hit my fingers then my hand will start to bleed"', () => {
+                getResult().should.have.property('response', o.expected);
+            });
+      });
+    });
+});
